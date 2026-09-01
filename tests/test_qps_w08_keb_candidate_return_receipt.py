59ebd0f1fd69d683d3e9fe4088fda173ad2d8c5c
GBOGEB <gerkotze.bonthuys@sckcen.be>
2026-09-01 20:49:33 +0200

    test(qps): update W08 peer lineage assertions

--- tests/test_qps_w08_keb_candidate_return_receipt.py
+++ tests/test_qps_w08_keb_candidate_return_receipt.py
@@ -24,14 +24,17 @@ class TestQpsW08KebCandidateReturnReceipt(unittest.TestCase):
         self.assertEqual(controls["formal_completion_delta"], 0)
         self.assertEqual(controls["bidder_compliance_delta"], 0)
 
-    def test_receipt_is_bound_to_merged_parent_and_active_peer_repair(self):
+    def test_receipt_is_bound_to_merged_parent_and_current_peer_lineage(self):
         data = load(RECEIPT)
         self.assertEqual(data["source_parent_pr"], "GBOGEB/CODEX#330")
         self.assertEqual(
             data["source_parent_merge_sha"], "a8525f61102d00cbbe5a7cbe832ac1573a86783c"
         )
-        self.assertEqual(data["peer_abacus_pr"], "GBOGEB/ABACUS#796")
-        self.assertEqual(data["peer_abacus_predecessor_pr"], "GBOGEB/ABACUS#795")
+        self.assertEqual(data["peer_abacus_pr"], "GBOGEB/ABACUS#805")
+        self.assertEqual(data["peer_abacus_merged_repair_pr"], "GBOGEB/ABACUS#802")
+        self.assertEqual(
+            data["peer_abacus_historical_predecessor_pr"], "GBOGEB/ABACUS#795"
+        )
 
     def test_incomplete_runtime_hash_binding_fails_closed(self):
         data = load(RECEIPT)
@@ -47,5 +50,10 @@ class TestQpsW08KebCandidateReturnReceipt(unittest.TestCase):
 
     def test_semantic_request_tracks_current_abacus_repair_lineage(self):
         request = load(REQUEST)
-        self.assertEqual(request["source_abacus_pr"], "GBOGEB/ABACUS#796")
-        self.assertEqual(request["source_abacus_predecessor_pr"], "GBOGEB/ABACUS#795")
+        self.assertEqual(request["source_abacus_pr"], "GBOGEB/ABACUS#805")
+        self.assertEqual(
+            request["source_abacus_merged_repair_pr"], "GBOGEB/ABACUS#802"
+        )
+        self.assertEqual(
+            request["source_abacus_historical_predecessor_pr"], "GBOGEB/ABACUS#795"
+        )
